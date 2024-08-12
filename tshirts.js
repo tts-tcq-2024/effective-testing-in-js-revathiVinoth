4fb92fe63f2a0019e002ca7a4f3cf950319b1d3e
revathiVinoth <revzz.1993@gmail.com>
2024-08-12 09:12:35 +0530

    update the test

--- tshirts.js
+++ tshirts.js
@@ -1,4 +1,4 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 
 function size(cms) {
     if (cms < 38) {
@@ -13,4 +13,5 @@ function size(cms) {
 expect(size(37)).equals('S');
 expect(size(40)).equals('M');
 expect(size(43)).equals('L');
+expect(size(38)).equals('M');
 console.log('All is well (maybe!)');
