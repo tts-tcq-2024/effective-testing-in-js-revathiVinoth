1681eeb643ae292dcada5366dedd0857a9e5a0a3
revathiVinoth <revzz.1993@gmail.com>
2024-08-26 09:06:50 +0530

    test misalgined

--- misaligned.js
+++ misaligned.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai')
 
+
 function calculateColorMap() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
@@ -18,10 +19,21 @@ function printColorMap(colorMap) {
     });
 }
 
+function constructExpectedColorMap() {
+    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+    const colorMap = [];
+    for (let i = 0; i < majorColors.length; i++) {
+        for (let j = 0; j < minorColors.length; j++) {
+            colorMap.push(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`)
+        }
+    }
+    return colorMap;
+}
 
-result = calculateColorMap();
-printColorMap(result)
-expect(result.length).equals(25);
-expect(result[0]).equals('0 | White | Blue'); // Test for correct alignment
-expect(result[24]).equals('24 | Violet | Slate');
+expectedColorMap = constructExpectedColorMap();
+observedColorMap = calculateColorMap();
+printColorMap(observedColorMap)
+expect(observedColorMap.length).equals(25);
+expect(observedColorMap).equals(expectedColorMap);
 console.log('All is well (maybe!)');
