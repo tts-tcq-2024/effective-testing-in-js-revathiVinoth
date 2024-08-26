4eb1f548601b03002fbde81eef1bfe8c69198868
revathiVinoth <revzz.1993@gmail.com>
2024-08-26 08:27:45 +0530

    updated

--- misaligned.js
+++ misaligned.js
@@ -1,16 +1,27 @@
-const {expect} = require('chai')
+const { expect } = require('chai')
 
-function print_color_map() {
+function calculateColorMap() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+    const colorMap = [];
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+            colorMap.push(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`)
         }
     }
-    return majorColors.length * minorColors.length;
+    return colorMap;
 }
 
-result = print_color_map();
-expect(result).equals(25);
+function printColorMap(colorMap) {
+    colorMap.forEach(element => {
+        console.log(element);
+    });
+}
+
+
+result = calculateColorMap();
+printColorMap(result)
+expect(result.length).equals(25);
+expect(result[0]).equals('0 | White | Blue'); // Test for correct alignment
+expect(result[24]).equals('24 | Violet | Slate');
 console.log('All is well (maybe!)');
